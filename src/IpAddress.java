60d05947c9b4b37401e97f1f4eb74ccf7ff74be7
Adore Ashish <adoreashish@gmail.com>
2016-03-23 10:42:12 +0530

    Added search file

---
+++ src/IpAddress.java
@@ -0,0 +1,12 @@
+import java.net.InetAddress;
+
+
+public class IpAddress {
+
+	public static void main(String[] args) throws Exception {
+
+		System.out.println(InetAddress.getLocalHost());
+		
+	}
+
+}
